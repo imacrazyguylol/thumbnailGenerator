b560c29fcf3e9d78ea4c101926bc91c40bdda4ce
ImACrazyGuyLol <github@imacrazyguy.xyz>
2022-07-29 01:41:37 -0400

    actual progress

--- index.jsx
+++ index.jsx
@@ -1,14 +1,28 @@
-const originalUnit = preferences.rulerUnits;
-preferences.rulerUnits = Units.INCHES;
+function replaceContents (newFile) {  
+    // =======================================================  
+    var idplacedLayerReplaceContents = stringIDToTypeID( "placedLayerReplaceContents" );  
+        var desc3 = new ActionDescriptor();  
+        var idnull = charIDToTypeID( "null" );  
+        desc3.putPath( idnull, new File( newFile ) );  
+        var idPgNm = charIDToTypeID( "PgNm" );  
+        desc3.putInteger( idPgNm, 1 );  
+    executeAction( idplacedLayerReplaceContents, desc3, DialogModes.NO );  
+    return app.activeDocument.activeLayer  
+};
 
-var artLayerRef = docRef.artLayers.add();
-artLayerRef.kind = LayerKind.TEXT;
+var originalUnit = preferences.rulerUnits;
+preferences.rulerUnits = Units.PIXELS;
 
-var textItemRef = artLayerRef.textItem;
-textItemRef.contents = "Hello World";
+var template = open(openDialog()[0]);
+activeDocument = template;
+var templateRef = template
 
-docRef = null;
+templateRef.activeLayer = templateRef.layers[2];
+var commentRef = templateRef.activeLayer.textItem;
+commentRef.contents = "BALLS";
+
+templateRef = null;
 artLayerRef = null;
-textItemRef = null
+textItemRef = null;
 
 app.preferences.rulerUnits = originalUnit;
