92549fdab957f1e74bd85f8de555b05c61bf30b4
ImACrazyGuyLol <github@imacrazyguy.xyz>
2022-08-02 21:30:11 -0400

    main.js is now for osu api testing

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-import "./index.jsx"
+const CLIENT_ID = 16965;
+const CLIENT_SECRET = "62M0ATIFPIgmyZzyLdGxfAWJZXW4w4vjOnjVo7ob"
+const { v2, auth, tools } = require('osu-api-extended')
 
-if( BridgeTalk.appName == "photoshop" ) {
-    main();
+
+const main = async () => {
+    // Auth via client
+    await auth.login(CLIENT_ID, CLIENT_SECRET);
+
+    fetch("https://osu.ppy.sh/api/v2/beatmaps/lookup", {
+    headers: {
+      Authorization: `Bearer ${CLIENT_SECRET}`
+    }
+});
 }
