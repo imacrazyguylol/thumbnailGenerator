c7270fda8878dc3cead1d6d80e9cd31421e5c5e8
github@imacrazyguy.xyz <github@imacrazyguy.xyz>
2022-08-05 19:31:33 -0400

    api stuff done (for terminal so far)

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const { v2, auth, tools } = require('osu-api-extended');
-const {CLIENT_ID, CLIENT_SECRET} = require('./secret.js');
+const { v2, auth } = require('osu-api-extended');
+const { CLIENT_ID, CLIENT_SECRET } = require('./secret.js');
 const prompt = require('prompt');
 
 function fileNameFromDir(dir){
@@ -12,15 +12,33 @@ const main = async () => {
     
     var schema = {
       properties: {
-        URL: {
+        url: {
+          name: 'url',
+          pattern: /https:\/\/osu\.ppy\.sh\/scores\/[a-zA-Z]+\/[0-9]+/i,
+          message: 'Must be a score URL i.e. https://osu.ppy.sh/scores/osu/4209503586',
           description: 'Enter the URL of the score. replay files are not supported yet. See github page for more info.',
           type: 'string',
           required: true
         }
       }
     };
-    const {url} = await prompt.get(schema);
-    console.log(url);
+    prompt.start();
+
+    //temp input method for use outside of PS
+    const scoreURL = (await prompt.get(schema)).url;
+    
+    function convertURL(url){
+      let idIndex = url.indexOf("/", 29);
+      let modeIndex = url.indexOf("/", 25);
+      let id = url.slice(idIndex + 1);
+      let mode = url.slice(modeIndex + 1, idIndex);
+      return [id, mode]
+    }
+
+    const [scoreID, scoreMode] = convertURL(scoreURL);
+
+    const score = await v2.scores.details(scoreID, scoreMode);
+    const {beatmap, beatmapset, user} = score;
 };
 
 main();
