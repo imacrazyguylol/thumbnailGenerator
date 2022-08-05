90c037da7fe38fc291a5d22397556424b6c562c7
github@imacrazyguy.xyz <github@imacrazyguy.xyz>
2022-08-05 17:41:32 -0400

    local

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { v2, auth, tools } = require('osu-api-extended');
-const {CLIENT_ID, CLIENT_SECRET} = require('./secret');
-const dialog = require('node-file-dialog');
+const {CLIENT_ID, CLIENT_SECRET} = require('./secret.js');
+const prompt = require('prompt');
 
 function fileNameFromDir(dir){
     return dir.slice(dir.lastIndexOf("/") + 1);
@@ -9,27 +9,18 @@ function fileNameFromDir(dir){
 const main = async () => {
     // Auth via client
     await auth.login(CLIENT_ID, CLIENT_SECRET);
-
-    var fileNames = [];
-    var beatmaps = [];
-    await dialog({type: 'open-files'})
-        .then((dir) => {
-            let soB;
-            let eoB;
-            for (let i = 0; i < dir.length; i++){
-                fileNames[i] = fileNameFromDir(dir[i]);
-                soB = fileNames[i].indexOf("-") + 2;
-                eoB = fileNames[i].lastIndexOf("]") + 1;
-                beatmaps[i] = {replay: fileNames[i], diffName: fileNames[i].slice(soB, eoB)};
-            }
-        })
-        .catch(err => console.log(err));
-    console.log(fileNames);
-    console.log(beatmaps[0]);
-
-    const data = await v2.beatmap.search(beatmaps[0].diffName);
-        console.log(data);
-        
+    
+    var schema = {
+      properties: {
+        URL: {
+          description: 'Enter the URL of the score. replay files are not supported yet. See github page for more info.',
+          type: 'string',
+          required: true
+        }
+      }
+    };
+    const {url} = await prompt.get(schema);
+    console.log(url);
 };
 
 main();
