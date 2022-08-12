58cd092b8bd77350f575df7f366ef4738853fc6a
imacrazyguylol <github@imacrazyguy.xyz>
2022-08-11 20:02:01 -0400

    background replace improvements/fixes

--- index.jsx
+++ index.jsx
@@ -1,3 +1,5 @@
+//constants syntax is this: Constant.VALUE
+
 function replaceContents (newFile) {  
     // =======================================================  
     var idplacedLayerReplaceContents = stringIDToTypeID( "placedLayerReplaceContents" );  
@@ -10,21 +12,46 @@ function replaceContents (newFile) {
     return app.activeDocument.activeLayer  
 };
 
+function convertToSmartObject () {
+    var desc108 = new ActionDescriptor();
+    var ref77 = new ActionReference();
+    ref77.putEnumerated( charIDToTypeID( "Mn  " ), charIDToTypeID( "MnIt" ), stringIDToTypeID( "newPlacedLayer" ) );
+    desc108.putReference( charIDToTypeID( "null" ), ref77 );
+    executeAction( charIDToTypeID( "slct" ), desc108, DialogModes.NO );
+};
+
+/*function uToPer (current, incTo) {
+    let d = incTo / current;
+    return d * 100;
+}*/
 var originalUnit = preferences.rulerUnits;
 preferences.rulerUnits = Units.PIXELS;
 
 var template = open(openDialog()[0]);
-activeDocument = template;
-var docRef = template
+app.activeDocument = template;
+var docRef = app.activeDocument;
 
-docRef.activeLayer = docRef.layers[2];
-var commentRef = docRef.activeLayer.textItem;
-commentRef.contents = "BALLS";
-commentRef = null;
+//docRef.activeLayer = docRef.layers[2];
+//var commentRef = docRef.activeLayer.textItem;
+//commentRef.contents = "BALLS";
+//commentRef = null;
+function backgroundReplace(){
+    //mostly working, just have to get the percentages consistent
 
-imageLayer = artLayers.add();
-var replacementFile = new File(openDialog()[0]);
-imageLayer = replaceContents(replacementFile);
+    docRef.activeLayer = docRef.artLayers.add();
+        var bkgRef = docRef.activeLayer;
+    var openedImage = openDialog()[0];
+    var replacementFile = new File(openedImage);
 
+    docRef.activeLayer.name = "background";
+        bkgRef.move(docRef, ElementPlacement.PLACEATEND);
+        convertToSmartObject();
+        replaceContents(replacementFile);
+        //docRef.activeLayer.resize(uToPer(/*whatever the standard is for beatmaps, or get from api maybe?*/, 1920), uToPer(/*same*/, 1080), AnchorPosition.MIDDLECENTER);
+        docRef.activeLayer.resize(100, 100, AnchorPosition.MIDDLECENTER)
+    bkgRef = null;
+}
+
+backgroundReplace();
 docRef = null;
 app.preferences.rulerUnits = originalUnit;
